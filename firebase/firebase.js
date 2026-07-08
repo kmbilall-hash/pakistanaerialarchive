a31c4e4e4652e0fb535638db6398264f12d24a82
usama1998-max <m.usamanaseer68@gmail.com>
2026-07-08 19:01:32 +0500

    firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,19 +1,35 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+<// Firebase App
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDM1nSzVZFS6qBr7FfFl_9BUJeZ8_PmBsQ",
-    authDomain: "pakistan-aerial-archive.firebaseapp.com",
-    projectId: "pakistan-aerial-archive",
-    storageBucket: "pakistan-aerial-archive.firebasestorage.app",
-    messagingSenderId: "663091537448",
-    appId: "1:663091537448:web:c7b06f37e22b93ef36b71d"
-  };
+// Authentication
+import {
+  getAuth,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+// Firestore
+import {
+  getFirestore,
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDM1nSzVZFS6qBr7FfFl_9BUJeZ8_PmBsQ",
+  authDomain: "pakistan-aerial-archive.firebaseapp.com",
+  projectId: "pakistan-aerial-archive",
+  storageBucket: "pakistan-aerial-archive.firebasestorage.app",
+  messagingSenderId: "663091537448",
+  appId: "1:663091537448:web:c7b06f37e22b93ef36b71d"
+};
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const app = initializeApp(firebaseConfig);
+
+// Export services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// Export functions we'll use later
+export { createUserWithEmailAndPassword, doc, setDoc };
