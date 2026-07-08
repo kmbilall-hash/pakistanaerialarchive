69f653cb4b8ccedff1b36f9c6382178e7fbbf9ad
usama1998-max <m.usamanaseer68@gmail.com>
2026-07-08 18:45:08 +0500

    firebase integration

---
+++ firebase/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDM1nSzVZFS6qBr7FfFl_9BUJeZ8_PmBsQ",
+    authDomain: "pakistan-aerial-archive.firebaseapp.com",
+    projectId: "pakistan-aerial-archive",
+    storageBucket: "pakistan-aerial-archive.firebasestorage.app",
+    messagingSenderId: "663091537448",
+    appId: "1:663091537448:web:c7b06f37e22b93ef36b71d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
